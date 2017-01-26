abaacefba6553883f052318eaa252ee832b9391a
Vitalii Kalchuk <v.sweak@gmail.com>
2017-01-26 16:03:50 +0200

    Add missing semicolons, update readme

--- src/store/store.test.js
+++ src/store/store.test.js
@@ -3,7 +3,7 @@ import { createStore } from 'redux';
 import rootReducer from '../reducers/rootReducer';
 import initialState from '../reducers/initialState';
 import * as courseActions from '../actions/courseActions';
- 
+
 describe('Store', function() {
   it('Should handle creating courses', function() {
     // arrange
@@ -11,17 +11,17 @@ describe('Store', function() {
     const course = {
       title: "Clean Code"
     };
- 
+
     // act
     const action = courseActions.createCourseSuccess(course);
     store.dispatch(action);
- 
+
     // assert
     const actual = store.getState().courses[0];
     const expected = {
       title: "Clean Code"
     };
- 
+
     expect(actual).toEqual(expected);
   });
-})
+});
